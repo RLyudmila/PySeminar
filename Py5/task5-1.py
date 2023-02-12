98509f2d04092c8c0c3b126d3cde9cf2f5926fda
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2023-02-12 19:03:20 +0300

    add task5

---
+++ Py5/task5-1.py
@@ -0,0 +1,16 @@
+# Напишите рекурсивную функцию для возведения числа a в степень b. 
+# Разрешается использовать только операцию умножения. Циклы использовать нельзя
+# Примеры/Тесты:
+# <function_name>(2,0) -> 1
+# <function_name>(2,1) -> 2
+# <function_name>(2,3) -> 8
+# <function_name>(2,4) -> 16
+
+def power(base, exp):
+    if (exp == 1):
+        return (base)
+    if (exp != 1):
+        return (base * power(base, exp - 1))
+base = int(input("Введите число: "))
+exp = int(input("Введите его степень: "))
+print("Результат возведения в степень равен:", power(base, exp))
