e3554192c37fe19191642a70de1ca8fd5fd16087
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2023-02-13 10:36:11 +0300

    editing task5-1

--- Py5/task5-1.py
+++ Py5/task5-1.py
@@ -6,11 +6,13 @@
 # <function_name>(2,3) -> 8
 # <function_name>(2,4) -> 16
 
-def power(base, exp):
-    if (exp == 1):
-        return (base)
-    if (exp != 1):
-        return (base * power(base, exp - 1))
-base = int(input("Введите число: "))
-exp = int(input("Введите его степень: "))
-print("Результат возведения в степень равен:", power(base, exp))
+def exponentiation(number, extent):
+    if (extent == 1):
+        return (number)
+    if (extent == 0):
+        return(1)
+    if (extent != 1):
+        return (number * exponentiation(number, extent - 1))
+number = int(input("Введите число: "))
+extent = int(input("Введите его степень: "))
+print("Результат возведения в степень равен:", exponentiation(number, extent))
